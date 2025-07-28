f6c1a0624e3ed2be0965642fcf2038199a8871f2
RobertaBS09 <robertabarbosas123@gmail.com>
2025-07-28 17:54:24 -0300

    Exercicios 01,02,03 feitos.

---
+++ Exercicio01.py
@@ -0,0 +1,11 @@
+base=(int(input('Digite o valor da base:')))
+altura=(int(input('Digite o valor da altura:')))
+
+
+area= (int(base* altura))
+
+
+perimetro=(int ((base+altura)*2))
+
+print(f'''Area: {area}
+Perimetro: {perimetro}''')
